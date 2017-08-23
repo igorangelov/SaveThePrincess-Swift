40fb562d2887fa894a19402c38b689327d3864b3
Igor Angelov <iangelov@touchandplay.fr>
2017-08-23 15:24:19 +0200

    [test] Create test for wall and soldier attack

--- SaveThePrincessTests/SaveThePrincessTests.swift
+++ SaveThePrincessTests/SaveThePrincessTests.swift
@@ -7,12 +7,28 @@
 //
 
 import XCTest
+
+import CoreData
 @testable import SaveThePrincess
 
 class SaveThePrincessTests: XCTestCase {
     
+    var viewController : MasterViewController?
+    
     override func setUp() {
         super.setUp()
+        
+        let storyboard = UIStoryboard(name: "Main",
+                                      bundle: Bundle.main)
+        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+        viewController = navigationController.topViewController as? MasterViewController
+        
+        UIApplication.shared.keyWindow!.rootViewController = viewController
+        
+        // Test and Load the View at the Same Time!
+        XCTAssertNotNil(navigationController.view)
+        XCTAssertNotNil(viewController?.view)
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
@@ -32,5 +48,26 @@ class SaveThePrincessTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+ 
+    func testCreateWall() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.createWall()
+        XCTAssertTrue((appDelegate.wall?.durability)! > 0 , "Wall creation failed")
+    }
     
+    func testAttack() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.createWall()
+        
+        let context = appDelegate._fetchedResultsController?.managedObjectContext
+        let newSoldier = Soldier(context: context!)
+        newSoldier.name = "Test Soldier"
+        newSoldier.color = UIColor.red.encode() as NSData
+        newSoldier.gender = Gender.male
+        newSoldier.age = (appDelegate.wall?.durability)!
+        
+        viewController?.soldierAttack(newSoldier)
+        XCTAssertTrue((appDelegate.wall?.durability)! == 0 , "Wall attack failed")
+    }
 }
